c71add4c270908fb6d4ff3234f2797be4208d620
Esteban Martini <emartini@comparaonline.com>
2017-04-26 16:21:09 -0300

    Convert modules from CommonJS to ES2015

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,17 @@
-module.exports = {
-  mod11Verifier: value => {
-    const number = value.replace(/\D/g, '');
+export const mod11Verifier = value => {
+  const number = value.replace(/\D/g, '');
 
-    const reversedStringArray = Array.from(number.toString()).reverse();
-    const sum = reversedStringArray.reduce((memo, digit, index) => {
-      const factor = (index % 6) + 2;
-      return memo + (parseInt(digit, 10) * factor);
-    }, 0);
+  const reversedStringArray = Array.from(number.toString()).reverse();
+  const sum = reversedStringArray.reduce((memo, digit, index) => {
+    const factor = (index % 6) + 2;
+    return memo + (parseInt(digit, 10) * factor);
+  }, 0);
 
-    const mod11 = 11 - (sum % 11);
-    let verifier = mod11;
-    if (mod11 === 11) verifier = 0;
-    if (mod11 === 10) verifier = 'k';
-    return verifier.toString();
-  },
-  cleanString: string => string.replace(/\W/g, '')
+  const mod11 = 11 - (sum % 11);
+  let verifier = mod11;
+  if (mod11 === 11) verifier = 0;
+  if (mod11 === 10) verifier = 'k';
+  return verifier.toString();
 };
+
+export const cleanString = string => string.replace(/\W/g, '');
