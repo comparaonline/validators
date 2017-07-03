33709e288beb1d9f416dba847533d409c2640692
Jacob Parra <jparra@comparaonline.com>
2017-07-03 18:14:26 -0400

    Adds br to validators export

--- src/validators.js
+++ src/validators.js
@@ -1,8 +1,10 @@
+import * as br from './br';
 import * as cl from './cl';
 import * as string from './string';
 import * as creditCard from './credit-card';
 
 export default {
+  br,
   cl,
   string,
   creditCard
