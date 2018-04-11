2a4bdeffcae03499cf272b8f3ffd05f3a74ca5a0
Mario Negrete <txus2k@gmail.com>
2018-04-11 15:28:21 -0300

    Add const `PLATE_MAX_LENGTH`

--- src/co.js
+++ src/co.js
@@ -1,8 +1,10 @@
 import { cleanString } from './utils';
 
+const PLATE_MAX_LENGTH = 6;
+
 export const plate = plateNumber => {
   const cleanedPlate = cleanString(plateNumber);
-  if (cleanedPlate.length !== 6) return false;
+  if (cleanedPlate.length !== PLATE_MAX_LENGTH) return false;
   const format = /^[a-zA-Z]{3}[0-9]{3}$/;
   return format.test(cleanedPlate);
 };
